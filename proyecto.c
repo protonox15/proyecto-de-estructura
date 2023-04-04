2860c1b987e41dea4512fc9ebb739cc7a053f01a
protonox15 <129882120+protonox15@users.noreply.github.com>
2023-04-04 14:51:46 -0500

    Update and rename ColaEjemplo.c to proyecto.c

---
+++ proyecto.c
@@ -0,0 +1,12 @@
+// proyecto de estructura de datos //
+// integrantes //
+// > ANGIE ALVARADO //
+// > XAVIER CARRASCO //
+// > JOAN VERA //
+
+#include <stdio.h>
+#include <stdlib.h>
+
+main(){
+    printf("Hola Mundo");
+}
